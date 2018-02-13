85c475547bfcc3baf9b0f3771bd88156dad5ced8
Mike <mjgaisser@gmail.com>
2018-02-12 20:39:41 -0600

    fixes for #2 and #3

--- ResultsForm.cs
+++ ResultsForm.cs
@@ -11,8 +11,11 @@
  * [UPD] Converted to current standards
  * [UPD] Implemented Idmr.Platform
  * [UPD] Removed Containers, probes, satellites, backdrops, etc from AI and Orders checks
+ * [UPD #3] AI messages changed "no AI" to "basic AI", removed failure indicator
+ * [UPD #2] results string[] replaced with List<string>
  */
 using Idmr.Platform;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
 
@@ -77,48 +80,43 @@ namespace Idmr.MissionVerify
 		{
 			Platform.Tie.Mission mission = new Platform.Tie.Mission(fsFile);
 			fsFile.Close();
-			int temp, iCraft = 0;	//consts and temp
-			int i = 0;		//string counter
-			string[] strResults = new string[50];
+			int iCraft = 0;	//consts and temp
+			List<string> results = new List<string>();
 			string playerFG = "";
 			bool bFail = false;
 			if (mission.FlightGroups.Count > Platform.Tie.Mission.FlightGroupLimit)
-				strResults[i++] = "**Warning, mission has more than 48 Flight Groups, mission is NOT flyable.";
+				results.Add("**Warning, mission has more than 48 Flight Groups, mission is NOT flyable.");
 			if (mission.Messages.Count == 0) 
-				strResults[i++] = "Mission has no in-flight messages.";
+				results.Add("Mission has no in-flight messages.");
 			if (mission.Messages.Count > Platform.Tie.Mission.MessageLimit)
-				strResults[i++] = "**Mission has more than 16 in-flight messages, errors will occur.";
+				results.Add("**Mission has more than 16 in-flight messages, errors will occur.");
 			if (mission.EndOfMissionMessages[0] == "")
-				strResults[i++] = "No Mission Complete message";
+				results.Add("No Mission Complete message");
 			foreach (Platform.Tie.FlightGroup fg in mission.FlightGroups)
 			{
 				iCraft += fg.NumberOfCraft;
 				if (fg.PlayerCraft != 0)
 				{
-					strResults[i++] = (bFail ? "**" : "") + "Flight Group " + fg.Name + " is player craft";
+					results.Add((bFail ? "**" : "") + "Flight Group " + fg.Name + " is player craft");
 					if (!bFail) playerFG = fg.Name;
 					bFail = true;
 				}
 			}
-			if (playerFG == "")
-				strResults[i++] = "**No player craft";
+			if (playerFG == "") results.Add("**No player craft");
 			if (iCraft > Platform.Tie.Mission.CraftLimit)
-				strResults[i++] = "*More than 28 craft, ensure not all exist concurrently";
+				results.Add("*More than 28 craft, ensure not all exist concurrently");
 			bFail = false;
 			foreach (Platform.Tie.FlightGroup fg in mission.FlightGroups)
 			{
 				if (fg.AI == 0 && (fg.CraftType < 0x1A || fg.CraftType > 0x1D) && (fg.CraftType < 0x37 || fg.CraftType > 0x3B) && fg.CraftType < 0x50)
-				{
-					strResults[i++] = (bFail ? "*" : "") + "Flight Group " + fg.Name + " has no AI" + (fg.Name == playerFG ? " (player" : "");
-					bFail = true;
-				}
+					results.Add("Flight Group " + fg.Name + " has basic AI" + (fg.Name == playerFG ? " (player" : ""));
 			}
 			bFail = false;
 			foreach (Platform.Tie.FlightGroup fg in mission.FlightGroups)
 			{
 				if (fg.Orders[0].Command == 0 && (fg.CraftType < 0x1A || fg.CraftType > 0x1D) && (fg.CraftType < 0x37 || fg.CraftType > 0x3B) && fg.CraftType < 0x50)
 				{
-					strResults[i] = (bFail ? "*" : "") + "Flight Group " + fg.Name + " has no orders" + (fg.Name == playerFG ? " (player" : "");
+					results.Add((bFail ? "*" : "") + "Flight Group " + fg.Name + " has no orders" + (fg.Name == playerFG ? " (player" : ""));
 					bFail = true;
 				}
 			}
@@ -127,41 +125,33 @@ namespace Idmr.MissionVerify
 				mission.GlobalGoals.Goals[0].Triggers[1].Condition == 10 &&
 				mission.GlobalGoals.Goals[0].T1AndOrT2 == false)
 			{
-				strResults[i++] = "**Primary Global goals are not completable";
+				results.Add("**Primary Global goals are not completable");
 			}
 			if (mission.GlobalGoals.Goals[1].Triggers[0].Condition != 10 &&
 				mission.GlobalGoals.Goals[1].Triggers[1].Condition == 10 &&
 				mission.GlobalGoals.Goals[1].T1AndOrT2 == false)
 			{
-				strResults[i++] = "**Secondary Global goals are not completable";
+				results.Add("**Secondary Global goals are not completable");
 			}
 			if (mission.GlobalGoals.Goals[2].Triggers[0].Condition != 10 &&
 				mission.GlobalGoals.Goals[2].Triggers[1].Condition == 10 &&
 				mission.GlobalGoals.Goals[2].T1AndOrT2 == false)
 			{
-				strResults[i++] = "**Bonus Global goals are not completable";
+				results.Add("**Bonus Global goals are not completable");
 			}
 			if (mission.Briefing.Length == 9998 || mission.Briefing.Length == 0x21C)
-			{
-				strResults[i++] = "*Briefing has default length";
-			}
+				results.Add("*Briefing has default length");
 			if (mission.Briefing.Events[0] == 9999 || mission.Briefing.Events[8] == 9999)
-			{
-				strResults[i++] = "*No briefing";
-			}
+				results.Add("*No briefing");
 			if (mission.BriefingQuestions.PreMissQuestions[0] == "")
-			{
-				strResults[i++] = "*No pre-mission Officer questions";
-			}
+				results.Add("*No pre-mission Officer questions");
 			if (mission.BriefingQuestions.PostMissQuestions[0] == "")
+				results.Add("No post-mission Officer questions");
+
+			string [] strRes = new string [results.Count];
+			for (int i = 0; i < strRes.Length; i++)
 			{
-				strResults[i++] = "No post-mission Officer questions";
-			}
-			//this portion merely reduces the size of the text returned
-			string [] strRes = new string [i];
-			for (temp = 0; temp < i; temp++)
-			{
-				strRes[temp] = strResults[temp];
+				strRes[i] = results[i];
 			}
 			return strRes;
 		}
@@ -170,36 +160,22 @@ namespace Idmr.MissionVerify
 		{
 			BinaryReader br = new BinaryReader(fsFile);
 			int FG, MESSAGE, temp, iCraft = 0;	//consts and temp
-			int i = 0;		//string counter
-			int iFG;		//position counters
-			string[] strResults = new string[50];
+			int iFG;        //position counters
+			List<string> results = new List<string>();
 			bool bFail = false;
 			int playerFG = -1;
 			fsFile.Position = 2;
 			FG = br.ReadInt16();
 			MESSAGE = br.ReadInt16();
 			if (FG > 46)
-			{
-				strResults[i] = "**Warning, more than 46 Flight Groups, mission may not be stable.";
-				i++;
-			}
+				results.Add("**Warning, more than 46 Flight Groups, mission may not be stable.");
 			if (MESSAGE == 0) 
-			{
-				strResults[i] = "Mission has no in-flight messages.";
-				i++;
-			}
+				results.Add("Mission has no in-flight messages.");
 			if (MESSAGE > 64)
-			{
-				strResults[i] = "**Mission has more than 64 in-flight messages, errors will occur.";
-				i++;
-			}
+				results.Add("**Mission has more than 64 in-flight messages, errors will occur.");
 			fsFile.Position = 1480 + FG * 1378 + MESSAGE * 116;
 			temp = fsFile.ReadByte();
-			if (temp == 0)
-			{
-				strResults[i] = "No Mission Complete message.";
-				i++;
-			}
+			if (temp == 0) results.Add("No Mission Complete message.");
 			for (iFG = 1; iFG <= FG; iFG++)
 			{
 				fsFile.Position = iFG * 1378 - 1131;
@@ -209,73 +185,46 @@ namespace Idmr.MissionVerify
 				temp = fsFile.ReadByte();
 				if (temp != 0)
 				{
-					strResults[i] = "Flight Group " + iFG + " is player craft.";
-					if (bFail == true) strResults[i] = "**" + strResults[i];
-					else playerFG = iFG;
-					i++;
+					results.Add((bFail ? "**" : "") + "Flight Group " + iFG + " is player craft.");
+					if (!bFail) playerFG = iFG;
 					bFail = true;
 				}
 			}
 			if (playerFG == -1)
-			{
-				strResults[i] = "**No player craft.";
-				i++;
-			}
+				results.Add("**No player craft.");
 			if (iCraft > 36)
-			{
-				strResults[i] = "*More than 36 craft in mission, ensure all do not exist concurrently.";
-				i++;
-			}
+				results.Add("*More than 36 craft in mission, ensure all do not exist concurrently.");
 			bFail = false;
 			for (iFG = 1; iFG <= FG; iFG++)
 			{
 				fsFile.Position = iFG * 1378 - 1125;
 				temp = fsFile.ReadByte();
-				if (temp == 255)
-				{
-					strResults[i] = "Flight Group " + iFG + " has no AI.";
-					if (iFG == playerFG) strResults[i] += " (player)";
-					if (bFail == true) strResults[i] = "*" + strResults[i];
-					i++;
-					bFail = true;
-				}
+				if (temp == 255) results.Add("Flight Group " + iFG + " has basic AI." + (iFG == playerFG ? " (player)" : ""));
 			}
-			bFail = false;
 			for (iFG = 1; iFG <= FG; iFG++)
 			{
 				fsFile.Position = iFG * 1378 - 1052;
 				temp = fsFile.ReadByte();
 				if (temp == 0)
 				{
-					strResults[i] = "Flight Group " + iFG + " has no orders.";
-					if (iFG == playerFG) strResults[i] += " (player)";
-					if (bFail == true) strResults[i] = "*" + strResults[i];
-					i++;
+					results.Add((bFail ? "*" : "") + "Flight Group " + iFG + " has no orders." + (iFG == playerFG ? " (player)" : ""));
 					bFail = true;
 				}
 			}
 			bFail = false;
 			fsFile.Position = 6324 + FG * 1378 + MESSAGE * 116;
 			temp = fsFile.ReadByte();
-			if (temp == 15)
-			{
-				strResults[i] = "*No briefing";
-				i++;
-			}
+			if (temp == 15) results.Add("*No briefing");
 			//check Descrip, different positions depending on platform
 			if (!BoP) fsFile.Position = fsFile.Length - 1024;
 			else fsFile.Position = fsFile.Length - 4096;
 			temp = fsFile.ReadByte();
-			if (temp == 0)
+			if (temp == 0) results.Add("*No Mission description");
+
+			string[] strRes = new string[results.Count];
+			for (int i = 0; i < strRes.Length; i++)
 			{
-				strResults[i] = "*No Mission description";
-				i++;
-			}
-			//this portion merely reduces the size of the text returned
-			string [] strRes = new string [i];
-			for (temp = 0; temp < i; temp++)
-			{
-				strRes[temp] = strResults[temp];
+				strRes[i] = results[i];
 			}
 			return strRes;
 		}
@@ -284,36 +233,19 @@ namespace Idmr.MissionVerify
 		{
 			BinaryReader br = new BinaryReader(fsFile);
 			int FG, MESSAGE, temp, iCraft = 0;	//consts and temp
-			int i = 0;		//string counter
-			int iFG;		//position counters
-			string[] strResults = new string[50];
+			int iFG;        //position counters
+			List<string> results = new List<string>();
 			bool bFail = false;
 			int playerFG = -1;
 			fsFile.Position = 2;
 			FG = br.ReadInt16();
 			MESSAGE = br.ReadInt16();
-			if (FG > 100)
-			{
-				strResults[i] = "**Warning, over 100 Flight Groups, mission may be unstable.";
-				i++;
-			}
-			if (MESSAGE == 0) 
-			{
-				strResults[i] = "Mission has no in-flight messages.";
-				i++;
-			}
-			if (MESSAGE > 65)
-			{
-				strResults[i] = "**Warning, over 65 in-flight messages, mission may be unstable.";
-				i++;
-			}
+			if (FG > 100) results.Add("**Warning, over 100 Flight Groups, mission may be unstable.");
+			if (MESSAGE == 0)  results.Add("Mission has no in-flight messages.");
+			if (MESSAGE > 65) results.Add("**Warning, over 65 in-flight messages, mission may be unstable.");
 			fsFile.Position = 12916 + FG * 3646 + MESSAGE * 162;
 			temp = fsFile.ReadByte();
-			if (temp == 0)
-			{
-				strResults[i] = "No Mission Complete message.";
-				i++;
-			}
+			if (temp == 0) results.Add("No Mission Complete message.");
 			for (iFG = 1; iFG <= FG; iFG++)
 			{
 				fsFile.Position = 5662 + iFG * 3646;
@@ -323,78 +255,48 @@ namespace Idmr.MissionVerify
 				temp = fsFile.ReadByte();
 				if (temp != 0)
 				{
-					strResults[i] = "Flight Group " + iFG + " is player craft.";
-					if (bFail == true) strResults[i] = "**" + strResults[i];
-					else playerFG = iFG;
-					i++;
+					results.Add((bFail ? "**" : "") + "Flight Group " + iFG + " is player craft.");
+					if (!bFail) playerFG = iFG;
 					fsFile.Position += 69;
 					if (fsFile.ReadByte() == 0) 
-					{
-						strResults[i] = "**Flight Group " + iFG + " has no mothership.";
-						i++;
-					}
+						results.Add("**Flight Group " + iFG + " has no mothership.");
 					bFail = true;
 				}
 			}
 			if (playerFG == -1)
-			{
-				strResults[i] = "**No player craft.";
-				i++;
-			}
+				results.Add("**No player craft.");
 			if (iCraft > 96)
-			{
-				strResults[i] = "*More than 96 craft. Ensure not all exist in the same region concurrently.";
-				i++;
-			}
+				results.Add("*More than 96 craft. Ensure not all exist in the same region concurrently.");
 			bFail = false;
 			for (iFG = 1; iFG <= FG; iFG++)
 			{
 				fsFile.Position = 5668 + iFG * 3646;
 				temp = fsFile.ReadByte();
-				if (temp == 0)
-				{
-					strResults[i] = "Flight Group " + iFG + " has no AI.";
-					if (iFG == playerFG) strResults[i] += " (player)";
-					if (bFail == true) strResults[i] = "*" + strResults[i];
-					i++;
-					bFail = true;
-				}
+				if (temp == 0) results.Add("Flight Group " + iFG + " has basic AI." + (iFG == playerFG ? " (player)" : ""));
 			}
-			bFail = false;
 			for (iFG = 1; iFG <= FG; iFG++)
 			{
 				fsFile.Position = 5756 + iFG * 3646;
 				temp = fsFile.ReadByte();
 				if (temp == 0)
 				{
-					strResults[i] = "Flight Group " + iFG + " has no orders.";
-					if (iFG == playerFG) strResults[i] += " (player)";
-					if (bFail == true) strResults[i] = "*" + strResults[i];
-					i++;
+					results.Add((bFail ? "*" : "") + "Flight Group " + iFG + " has no orders." + (iFG == playerFG ? " (player)" : ""));
 					bFail = true;
 				}
 			}
 			bFail = false;
 			fsFile.Position = 17760 + FG * 3646 + MESSAGE * 162;
 			temp = fsFile.ReadByte();
-			if (temp == 15)
-			{
-				strResults[i] = "*No briefing.";
-				i++;
-			}
+			if (temp == 15) 	results.Add("*No briefing.");
 			//check Descrip
 			fsFile.Position = fsFile.Length - 12288;
 			temp = fsFile.ReadByte();
-			if (temp == 0)
+			if (temp == 0) 	results.Add("*No Mission description.");
+
+			string[] strRes = new string[results.Count];
+			for (int i = 0; i < strRes.Length; i++)
 			{
-				strResults[i] = "*No Mission description.";
-				i++;
-			}
-			//this portion merely reduces the size of the text returned
-			string [] strRes = new string [i];
-			for (temp = 0; temp < i; temp++)
-			{
-				strRes[temp] = strResults[temp];
+				strRes[i] = results[i];
 			}
 			return strRes;
 		}
